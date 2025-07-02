7bf4d3edf74d9a7982136764c05ea55fdf918c4e
unknown <hamdanasahar@gmail.com>
2025-07-02 13:36:57 +0100

    first commit

---
+++ budget/budget/src/main/java/com/onetech/budget/models/Client.java
@@ -0,0 +1,17 @@
+package com.onetech.budget.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String prenom;
+}
